e04b48b50371165eb1aa9e032b37720f2981ea8a
Christopher Sosa <urri_@hotmail.com>
2022-10-14 10:51:20 -0500

    Obtener nombre de admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,12 @@
 
 var express = require("express");
 var adminController = require("../controllers/AdminController");
+var auth = require("../middlewares/authenticate");
 
 var api = express.Router();
 
 api.post("/registro_admin", adminController.registro_admin);
 api.post("/login_admin", adminController.login_admin);
+api.get("/obtener_admin/:id", auth.auth, adminController.obtener_admin);
+
 module.exports = api;
